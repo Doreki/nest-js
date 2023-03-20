45fefa9b07b442379922b064f35a480df7fd3862
Doreki <idh0325@naver.com>
2023-03-21 08:58:46 +0900

    user 컨트롤러

--- my-project/src/app.controller.ts
+++ my-project/src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Redirect('https://naver.com')
   @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
--- my-project/src/users/users.controller.ts
+++ my-project/src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    const { name, email } = createUserDto;
+
+    return `유저를 생성했습니다. 이름: ${name} , 이메일: ${email}`;
+  }
+}
