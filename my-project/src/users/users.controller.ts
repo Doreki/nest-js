38a9dd9adcd5d16592626e2e123d3b7ff9dd8f5a
Doreki <idh0325@naver.com>
2023-03-22 08:59:03 +0900

    유저 서비스

--- my-project/src/users/users.controller.ts
+++ my-project/src/users/users.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { UserInfo } from 'os';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,7 +12,20 @@ export class UsersController {
   }
 
   @Post('/email-verify')
-  async verifyEmail(@Query() dto:VerifyEmailDto):Promise<string>{
-    console.log(this.$destroy()return;
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
   }
 }
