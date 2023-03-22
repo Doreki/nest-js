70e0bbc91814131fb03df165034d42226eef22f4
Doreki <idh0325@naver.com>
2023-03-22 20:48:51 +0900

    프로바이더

--- my-project/src/app.module.ts
+++ my-project/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { UsersController } from './users/users.controller';
-import { ApiController } from './api/api.controller';
+import { UsersService } from './users/users.service';
+import { EmailService } from './email/email.service';
 
 @Module({
   imports: [],
-  controllers: [ApiController, AppController, UsersController],
-  providers: [AppService],
+  controllers: [UsersController],
+  providers: [UsersService, EmailService],
 })
 export class AppModule {}
---
+++ my-project/src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { UserInfo } from 'os';
+import { EmailService } from 'src/email/email.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private emailService: EmailService) {}
+
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExists(email);
+
+    const signupVerifyToken = uuid.v1();
+
+    await this.saveUser(name, email, password, signupVerifyToken);
+    await this.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+
+  private checkUserExists(email: string) {
+    return false;
+  }
+
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    retrun;
+  }
+
+  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+    await this.emailService.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+
+  async verifyEmail(signupVerifyToken: string): Promise<string> {
+    throw new Error('Method not implemented');
+  }
+
+  async login(email: string, password: string): Promise<string> {
+    throw new Error('Method not implemented');
+  }
+
+  async getUserInfo(userId: string): Promise<UserInfo> {
+    throw new Error('Method not implemented');
+  }
+}
