ccb64b0fcf70ab903fdb4f273b19c82e23c24667
Doreki <idh0325@naver.com>
2023-03-23 09:04:49 +0900

    모듈,환경변수

--- my-project/src/app.module.ts
+++ my-project/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { EmailService } from './email/email.service';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { UsersModule } from './users/users.module';
+import { validationSchema } from './config/validation-schema';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
