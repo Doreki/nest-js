be9acc6b7ef509d4641e6252ef35fc6dca1a058c
Doreki <idh0325@naver.com>
2023-04-09 23:02:05 +0900

    17장

--- my-project/src/cqrs/create.user.handler.ts
+++ my-project/src/cqrs/create.user.handler.ts
@@ -2,7 +2,5 @@ import { ICommandHandler } from '@nestjs/cqrs';
 import { CreateUserCommand } from './create.user.command';
 
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
-  async execute(command: CreateUserCommand): Promise<any> {
-    const { name, email, password } = command;
-  }
+  async execute(command: CreateUserCommand): Promise<any> {}
 }
